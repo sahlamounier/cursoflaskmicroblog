ef85c3e28ba3e1a45e791089f6db3506fac2ab54
sahlamounier <sabrinalfmoura@gmail.com>
2024-02-18 20:46:21 -0300

    first commit

---
+++ app/routes.py
@@ -0,0 +1,19 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/index')
+
+def index():
+    nome = 'Lamounier'
+    dados = {"profissao": "Professora", 
+             "canal" : "Profa. Alba Lopes"}
+    return render_template(
+        'index.html', 
+        nome=nome,
+        dados=dados
+    )
+    
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
